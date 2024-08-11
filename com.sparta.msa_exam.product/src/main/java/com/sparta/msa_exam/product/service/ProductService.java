657cb1ccd04b2becb3fce28fb994c10dab2a5de1
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-08-11 13:39:02 +0900

    finish: 7.1, 7.2

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/service/ProductService.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/service/ProductService.java
@@ -4,6 +4,8 @@ import com.sparta.msa_exam.product.dto.ProductDto;
 import com.sparta.msa_exam.product.entity.Product;
 import com.sparta.msa_exam.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -11,19 +13,22 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-    @CachePut(cacheNames = "productAllCache", key = "#result.productId")
+    @CachePut(cacheNames = "productSaveCache", key = "#result.productId")
+    @CacheEvict(cacheNames = "productAllCache", allEntries = true)
     public Product save(Product product) {
         return productRepository.save(product);
     }
 
     @Cacheable(cacheNames = "productAllCache", key = "methodName")
     public List<ProductDto> findAll() {
+        log.info("ProductService.findAll()");
         return productRepository.findAll()
                 .stream().map(ProductDto::fromEntity)
                 .toList();
